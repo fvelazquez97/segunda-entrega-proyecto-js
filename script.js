16780f89cc1a9066303fd204af35ee8f38f5e8ad
fvelazquez <fvelazquez@outlook.com.ar>
2022-10-23 17:49:29 -0300

    agregue do while

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 console.log("BIENVENIDO A LA CALCULADORA DE GASTOS POR PERSONA");
 alert("BIENVENIDO A LA CALCULADORA DE GASTOS POR PERSONA");
 
-let cantidadIntegrantes = Number(prompt("Cuantas personas son: "));
+let cantidadIntegrantes
 
-while (isNaN(cantidadIntegrantes)) {
-  alert("Ingrese solo numeros");
-  cantidadIntegrantes = Number(prompt("Cuantas personas son: "));
-}
-
-const integrantes = [];
-
-for (let i = 0; i < cantidadIntegrantes; i++) {
-  const obj = {
-    id: i,
-    nombre: prompt("Ingrese el nombre de la persona: "),
-    dinero: Number(prompt(`Ingrese cuanto dinero puso: `)),
-  };
-
-  integrantes.push(obj);
-}
-
-let dineroTotal = 0;
-
-for (let i = 0; i < integrantes.length; i++) {
-  let element = integrantes[i].dinero;
-  dineroTotal = dineroTotal + element;
-}
-
-let dineroCadaUno = dineroTotal / cantidadIntegrantes;
-
-for (let i = 0; i < integrantes.length; i++) {
-  let resultado = integrantes[i].dinero - dineroCadaUno;
-
-  if (resultado < 0) {
-    console.log(integrantes[i].nombre, "Tiene que poner $", resultado * -1);
-  } else if (resultado == 0) {
-    console.log(integrantes[i].nombre, "No tiene que poner mas dinero");
-  } else {
-    console.log("A", integrantes[i].nombre, "le tienen que dar $", resultado);
+do {
+  cantidadIntegrantes = Number(prompt("Indique cuantas personas son: "));
+  
+  while (isNaN(cantidadIntegrantes)) {
+    alert("Ingrese solo numeros");
+    cantidadIntegrantes = Number(prompt("Cuantas personas son: "));
   }
-}
+  
+  const integrantes = [];
+  
+  for (let i = 0; i < cantidadIntegrantes; i++) {
+    const obj = {
+      id: i,
+      nombre: prompt("Ingrese el nombre de la persona: "),
+      dinero: Number(prompt(`Ingrese cuanto dinero puso: `)),
+    };
+  
+    integrantes.push(obj);
+  }
+  
+  let dineroTotal = 0;
+  
+  for (let i = 0; i < integrantes.length; i++) {
+    let element = integrantes[i].dinero;
+    dineroTotal = dineroTotal + element;
+  }
+  
+  let dineroCadaUno = dineroTotal / cantidadIntegrantes;
+  
+  for (let i = 0; i < integrantes.length; i++) {
+    let resultado = integrantes[i].dinero - dineroCadaUno;
+  
+    if (resultado < 0) {
+      console.log(integrantes[i].nombre, "Tiene que poner $", resultado * -1);
+    } else if (resultado == 0) {
+      console.log(integrantes[i].nombre, "No tiene que poner mas dinero");
+    } else {
+      console.log("A", integrantes[i].nombre, "le tienen que dar $", resultado);
+    }
+  }
+
+  cantidadIntegrantes = Number(prompt("Si desea salir ingrese 0, de lo contrario ingrese cualquier numero para volver a comenzar"));
+  
+} while (cantidadIntegrantes != 0);
