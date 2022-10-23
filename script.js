e2edd8feb3887da91879068387926a283f84755d
fvelazquez <fvelazquez@outlook.com.ar>
2022-10-23 17:39:33 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("BIENVENIDO A LA CALCULADORA DE GASTOS POR PERSONA");
+alert("BIENVENIDO A LA CALCULADORA DE GASTOS POR PERSONA");
+
+let cantidadIntegrantes = Number(prompt("Cuantas personas son: "));
+
+while (isNaN(cantidadIntegrantes)) {
+  alert("Ingrese solo numeros");
+  cantidadIntegrantes = Number(prompt("Cuantas personas son: "));
+}
+
+const integrantes = [];
+
+for (let i = 0; i < cantidadIntegrantes; i++) {
+  const obj = {
+    id: i,
+    nombre: prompt("Ingrese el nombre de la persona: "),
+    dinero: Number(prompt(`Ingrese cuanto dinero puso: `)),
+  };
+
+  integrantes.push(obj);
+}
+
+let dineroTotal = 0;
+
+for (let i = 0; i < integrantes.length; i++) {
+  let element = integrantes[i].dinero;
+  dineroTotal = dineroTotal + element;
+}
+
+let dineroCadaUno = dineroTotal / cantidadIntegrantes;
+
+for (let i = 0; i < integrantes.length; i++) {
+  let resultado = integrantes[i].dinero - dineroCadaUno;
+
+  if (resultado < 0) {
+    console.log(integrantes[i].nombre, "Tiene que poner $", resultado * -1);
+  } else if (resultado == 0) {
+    console.log(integrantes[i].nombre, "No tiene que poner mas dinero");
+  } else {
+    console.log("A", integrantes[i].nombre, "le tienen que dar $", resultado);
+  }
+}
